cd042abfd4273766595f79ba091e578b2e9a5dce
Ashish Jha <ashishjhaofficial@outlook.com>
2018-01-09 02:16:03 +0530

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,46 @@
+const OD = [
+  {
+    name: "dog",
+    fact: "A dog has 4 legs. kuch bdhia bc",
+    photo: " dog ki photo ka link ya download krke",
+    fly: "no"
+  },
+  {
+    name: "cat",
+    fact: "A dog has 4 legs. kuch bdhia bc",
+    photo: " dog ki photo ka link ya download krke",
+    fly: "no"
+  },
+  {
+    name: "swan",
+    fact: "A dog has 4 legs. kuch bdhia bc",
+    photo: " dog ki photo ka link ya download krke",
+    fly: "yes"
+  },
+  {
+    name: "butterfly",
+    fact: "A dog has 4 legs. kuch bdhia bc",
+    photo: " dog ki photo ka link ya download krke",
+    fly: "yes"
+  },
+  {
+    name: "elephant",
+    fact: "A dog has 4 legs. kuch bdhia bc",
+    photo: " dog ki photo ka link ya download krke",
+    fly: "no"
+  },
+  {
+    name: "helicopter",
+    fact: "A dog has 4 legs. kuch bdhia bc",
+    photo: " dog ki photo ka link ya download krke",
+    fly: "yes"
+  },
+  {
+    name: "superman",
+    fact: "A dog has 4 legs. kuch bdhia bc",
+    photo: " dog ki photo ka link ya download krke",
+    fly: "yes"
+  }
+];
+
+module.exports.OD = OD;
