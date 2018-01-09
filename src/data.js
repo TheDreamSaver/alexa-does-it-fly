7d623b750b524a63a6b2d3dfe8e73089586ff8a1
Ashish Jha <ashishjhaofficial@outlook.com>
2018-01-10 02:00:27 +0530

    skill ready

--- src/data.js
+++ src/data.js
@@ -1,46 +1,140 @@
 const OD = [
   {
-    name: "dog",
-    fact: "A dog has 4 legs. kuch bdhia bc",
-    photo: " dog ki photo ka link ya download krke",
+    name: "Dog",
+    fact: "The Basenji is the only breed of dog that can't bark, but they can yodel!",
+    photoSmall: "https://s3.amazonaws.com/doesitfly/smallImageUrl/dog.jpg",
+    photoBig: "https://s3.amazonaws.com/doesitfly/largeImageUrl/dog.jpg",
+    mp3: "https://s3.amazonaws.com/doesitfly/mp3/dog.mp3", 
     fly: "no"
   },
   {
-    name: "cat",
-    fact: "A dog has 4 legs. kuch bdhia bc",
-    photo: " dog ki photo ka link ya download krke",
+    name: "Aeroplane",
+    fact: "The oxygen in an airplane’s emergency oxygen masks lasts for only about 15 minutes.",
+    photoSmall: "https://s3.amazonaws.com/doesitfly/smallImageUrl/aeroplane.png",
+    photoBig: "https://s3.amazonaws.com/doesitfly/largeImageUrl/aeroplane.png",
+    mp3: "https://s3.amazonaws.com/doesitfly/mp3/aeroplane.mp3", 
+    fly: "yes"
+  },
+  {
+    name: "Bee",
+    fact: "Honey bees fly at 15 miles per hour. Honey bees' wings stroke 11,400 times per minute, thus making their distinctive buzz.",
+    photoSmall: "https://s3.amazonaws.com/doesitfly/smallImageUrl/bee.jpg",
+    photoBig: "https://s3.amazonaws.com/doesitfly/largeImageUrl/bee.jpg",
+    mp3: "https://s3.amazonaws.com/doesitfly/mp3/bee.mp3", 
+    fly: "yes"
+  },
+  {
+    name: "Butterfly",
+    fact: "Butterflies taste with their feet and their eyes are made of 6,000 lenses and can see ultraviolet light.",
+    photoSmall: "https://s3.amazonaws.com/doesitfly/smallImageUrl/butterfly.jpg",
+    photoBig: "https://s3.amazonaws.com/doesitfly/largeImageUrl/butterfly.jpg",
+    mp3: "https://s3.amazonaws.com/doesitfly/mp3/butterfly.mp3", 
+    fly: "yes"
+  },
+  {
+    name: "Cat",
+    fact: "A cat's nose is ridged with a unique pattern, just like a human fingerprint.",
+    photoSmall: "https://s3.amazonaws.com/doesitfly/smallImageUrl/cat.jpg",
+    photoBig: "https://s3.amazonaws.com/doesitfly/largeImageUrl/cat.jpg",
+    mp3: "https://s3.amazonaws.com/doesitfly/mp3/cat.mp3", 
     fly: "no"
   },
   {
-    name: "swan",
-    fact: "A dog has 4 legs. kuch bdhia bc",
-    photo: " dog ki photo ka link ya download krke",
+    name: "Crow",
+    fact: "When a crow dies, its neighbours may throw a funeral to try and learn about potential dangers to their own lives.",
+    photoSmall: "https://s3.amazonaws.com/doesitfly/smallImageUrl/crow.jpg",
+    photoBig: "https://s3.amazonaws.com/doesitfly/largeImageUrl/crow.jpg",
+    mp3: "https://s3.amazonaws.com/doesitfly/mp3/crow.mp3", 
     fly: "yes"
   },
   {
-    name: "butterfly",
-    fact: "A dog has 4 legs. kuch bdhia bc",
-    photo: " dog ki photo ka link ya download krke",
-    fly: "yes"
-  },
-  {
-    name: "elephant",
-    fact: "A dog has 4 legs. kuch bdhia bc",
-    photo: " dog ki photo ka link ya download krke",
+    name: "Cheetah",
+    fact: "The cheetah can reach its top speed in just 3 seconds and is the only cat that cannot completely retract its claws.",
+    photoSmall: "https://s3.amazonaws.com/doesitfly/smallImageUrl/cheetah.jpg",
+    photoBig: "https://s3.amazonaws.com/doesitfly/largeImageUrl/cheetah.jpg",
+    mp3: "https://s3.amazonaws.com/doesitfly/mp3/cheetah.mp3", 
     fly: "no"
   },
   {
-    name: "helicopter",
-    fact: "A dog has 4 legs. kuch bdhia bc",
-    photo: " dog ki photo ka link ya download krke",
+    name: "Dove",
+    fact: "Dove Mostly live happily in woodlands and tropical forests and the female dove birds mostly lay two eggs at a time.",
+    photoSmall: "https://s3.amazonaws.com/doesitfly/smallImageUrl/dove.jpg",
+    photoBig: "https://s3.amazonaws.com/doesitfly/largeImageUrl/dove.jpg",
+    mp3: "https://s3.amazonaws.com/doesitfly/mp3/dove.mp3", 
     fly: "yes"
   },
   {
-    name: "superman",
-    fact: "A dog has 4 legs. kuch bdhia bc",
-    photo: " dog ki photo ka link ya download krke",
+    name: "Eagle",
+    fact: "In most eagle species, females are larger and stronger than males. Currently, there are about 60 species of eagles.",
+    photoSmall: "https://s3.amazonaws.com/doesitfly/smallImageUrl/eagle.jpg",
+    photoBig: "https://s3.amazonaws.com/doesitfly/largeImageUrl/eagle.jpg",
+    mp3: "https://s3.amazonaws.com/doesitfly/mp3/eagle.mp3", 
     fly: "yes"
-  }
+  },
+  {
+    name: "Dolphin",
+    fact: "Dolphins are carnivores. The Killer Whale, also known as Orca, is actually a type of dolphin.",
+    photoSmall: "https://s3.amazonaws.com/doesitfly/smallImageUrl/dolphin.jpg",
+    photoBig: "https://s3.amazonaws.com/doesitfly/largeImageUrl/dolphin.jpg",
+    mp3: "https://s3.amazonaws.com/doesitfly/mp3/dolphin.mp3", 
+    fly: "no"
+  },
+  {
+    name: "Elephant",
+    fact: "Elephants are the largest land mammals. Wild elephants live in families called herds and the leader of the herd is usually the oldest female – she is called the Matriarch.",
+    photoSmall: "https://s3.amazonaws.com/doesitfly/smallImageUrl/elephant.jpg",
+    photoBig: "https://s3.amazonaws.com/doesitfly/largeImageUrl/elephant.jpg",
+    mp3: "https://s3.amazonaws.com/doesitfly/mp3/elephant.mp3", 
+    fly: "no"
+  },
+  {
+    name: "Hang Glider",
+    fact: "The record for the longest distance flown by a hang glider is 475 miles made by Dustin Martin in 2012.",
+    photoSmall: "https://s3.amazonaws.com/doesitfly/smallImageUrl/glider.jpg",
+    photoBig: "https://s3.amazonaws.com/doesitfly/largeImageUrl/glider.jpg",
+    mp3: "https://s3.amazonaws.com/doesitfly/mp3/glider.mp3", 
+    fly: "yes"
+  },
+  {
+    name: "Helicopter",
+    fact: "Most helicopters have two rotors, one on the top and one on the tail and the fastest recorded speed of a helicopter is roughly 248 miles per hour.",
+    photoSmall: "https://s3.amazonaws.com/doesitfly/smallImageUrl/helicopter.jpg",
+    photoBig: "https://s3.amazonaws.com/doesitfly/largeImageUrl/helicopter.jpg",
+    mp3: "https://s3.amazonaws.com/doesitfly/mp3/helicopter.mp3", 
+    fly: "yes"
+  },
+  {
+    name: "Lion",
+    fact: "A lion’s heels don’t touch the ground when it walks.",
+    photoSmall: "https://s3.amazonaws.com/doesitfly/smallImageUrl/lion.jpg",
+    photoBig: "https://s3.amazonaws.com/doesitfly/largeImageUrl/lion.jpg",
+    mp3: "https://s3.amazonaws.com/doesitfly/mp3/lion.mp3", 
+    fly: "no"
+  },
+  {
+    name: "Llama",
+    fact: "Llamas are members of the camelid, or camel, family and can grow as much as 6 feet tall.",
+    photoSmall: "https://s3.amazonaws.com/doesitfly/smallImageUrl/llama.jpg",
+    photoBig: "https://s3.amazonaws.com/doesitfly/largeImageUrl/llama.jpg",
+    mp3: "https://s3.amazonaws.com/doesitfly/mp3/llama.mp3", 
+    fly: "no"
+  },
+  {
+    name: "Swan",
+    fact: "A male swan is called a Cob, and a female swan is called a Pen.",
+    photoSmall: "https://s3.amazonaws.com/doesitfly/smallImageUrl/swan.jpg",
+    photoBig: "https://s3.amazonaws.com/doesitfly/largeImageUrl/swan.jpg",
+    mp3: "https://s3.amazonaws.com/doesitfly/mp3/swan.mp3", 
+    fly: "yes"
+  },
+  {
+    name: "Zebra",
+    fact: "Every zebra has a unique pattern of black and white stripes. A zebra’s stripy coat is thought to disperse more than 70 per cent of incoming heat",
+    photoSmall: "https://s3.amazonaws.com/doesitfly/smallImageUrl/zebra.jpg",
+    photoBig: "https://s3.amazonaws.com/doesitfly/largeImageUrl/zebra.jpg",
+    mp3: "https://s3.amazonaws.com/doesitfly/mp3/zebra.mp3", 
+    fly: "no"
+  },
 ];
 
 const AR = Array.from(Array(OD.length).keys());
